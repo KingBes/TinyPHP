7955e0d80499d93613166fc1fffcdcbd9f8a35df
KingBes <48203677+KingBes@users.noreply.github.com>
2026-06-30 18:03:03 +0800

    Update ha.php

--- test/var/ha.php
+++ test/var/ha.php
@@ -1,17 +1,15 @@
-<?php
-
 class Main {
     public function main(): void {
         $a = <<<HTML
-<h1>Hello, World!</h1>
-<div class="" title=""></div>
-<p></p>
-HTML;
+        <h1>Hello, World!</h1>
+        <div class="" title=""></div>
+        <p></p>
+        HTML;
         $b = <<<CSS
         body {
-            
+
         }
-CSS;
+        CSS;
         echo "Hello, World!\n";
         var_dump($a);
     }
